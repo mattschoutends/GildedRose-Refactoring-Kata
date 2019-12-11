25c07f47e6739fef3566b8ce28c88efe25ebb899
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2019-12-11 11:25:26 -0600

    add test for decreasing quality

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -4,12 +4,18 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo_quality_neve_below_zero(self):
+    def test_foo_quality_never_below_zero(self):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("foo", items[0].name)
-        self.assertEquals(0, items[0].quality)
+        self.assertEqual("foo", items[0].name)
+        self.assertEqual(0, items[0].quality)
+
+    def test_foo_quality_decreases_by_one(self):
+        items = [Item("foo", 0, 1)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
