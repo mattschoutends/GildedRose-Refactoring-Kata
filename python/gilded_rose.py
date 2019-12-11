ff3e00d980a4ed4f0237278dfeabaf17da5d1d66
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2019-12-11 11:44:58 -0600

    Refactor our quality over 50

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -16,11 +16,9 @@ class GildedRose(object):
                     item.quality = item.quality + 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                            item.quality = item.quality + 1
                         if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                            item.quality = item.quality + 1
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
@@ -32,8 +30,9 @@ class GildedRose(object):
                     else:
                         item.quality = item.quality - item.quality
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    item.quality = item.quality + 1
+            if item.quality > 50:
+                item.quality = 50
 
 
 class Item:
