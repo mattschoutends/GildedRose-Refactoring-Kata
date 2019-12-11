d6e391d27761af359abfefd9539831e7e36f16cf
rdmagm062699 <rob.miller@proactivedigitalinsights.com>
2019-12-11 11:23:02 -0600

    Add assertion that quality never below zero

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -4,11 +4,12 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
+    def test_foo_quality_neve_below_zero(self):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals("foo", items[0].name)
+        self.assertEquals(0, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
